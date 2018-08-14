de79f028edf3fa012ae0d91d5cf6e8a835de2191
gxsuyou <gxsuyou@126.com>
2018-08-14 15:05:58 +0800

    ===

--- PC/js/ONE.js
+++ PC/js/ONE.js
@@ -1,15 +1,23 @@
 $(function() {
-	$('.android').on({
-		mouseover: function() {
-			$(this).css('background-image', 'url(../img/android3.png)');
-		},
-		mouseout: function() {
-			$(this).css('background-image', 'url(../img/android2.png)');
-		}
+
+  $('.downLoad').mouseover(function(){
+		$('.qrcodeImg').css('display','block');
+	});
+
+	$('.downLoad').mouseout(function(){
+			$('.qrcodeImg').css('display','none');
 	})
-	$('.android,.downLoad').click(function() {
-		window.location.href = "http://www.oneyouxi.com.cn:8000/download?data=9"
-	})
+
+
+	$('.android').click(function() {
+		window.location.href = "https://admin.oneyouxi.com.cn/www/download/onegame.apk"
+	});
+	$('.ios').click(function() {
+	 window.location.href = "https://admin.oneyouxi.com.cn/www/download/onegame.ipa"
+ });
+
+
+
 	$('.nav').children('div').eq(2).click(function() {
 		window.location.href = "about_us.html"
 	})
